a32bce08f55aa2ee5cd851266742b8537d016c12
xrvel <dy.xrvel@gmail.com>
2017-07-25 16:51:30 +0700

    Add files via upload

---
+++ test_trades.php
@@ -0,0 +1,23 @@
+<?php
+require_once(dirname(__FILE__).'/includes/library.php');
+
+date_default_timezone_set('Asia/Jakarta');
+
+$curl = my_curl_get('https://vip.bitcoin.co.id/api/btc_idr/trades');
+
+$response = $curl['response'];
+if ('' != $response) {
+	$response = json_decode($response, true);
+	//echo '<pre>';var_dump($response);echo '</pre>';
+
+	foreach ($response as $data) {
+		echo '<div style="padding:1em">';
+		echo '<div>Date : '.date('r', $data['date']).'</div>';
+		echo '<div>Price : IDR '.number_format($data['price'], 0, ',', '.').'</div>';
+		echo '<div>Amount : '.number_format($data['amount'], 8, ',', '.').'</div>';
+		echo '<div>Type : '.$data['type'].'</div>';
+		echo '<div>Tid : '.$data['tid'].'</div>';
+		echo '</div>';
+	}
+}
+?>

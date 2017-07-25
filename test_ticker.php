9542e6956c215459b39f2e9c4ed22c8ad90c9872
xrvel <dy.xrvel@gmail.com>
2017-07-25 16:43:37 +0700

    Update test_ticker.php

--- test_ticker.php
+++ test_ticker.php
@@ -1,11 +1,22 @@
 <?php
 require_once(dirname(__FILE__).'/includes/library.php');
 
+date_default_timezone_set('Asia/Jakarta');
+
 $curl = my_curl_get('https://vip.bitcoin.co.id/api/btc_idr/ticker');
 
 $response = $curl['response'];
 if ('' != $response) {
 	$response = json_decode($response, true);
-	echo '<pre>';var_dump($response);echo '</pre>';
+	//echo '<pre>';var_dump($response);echo '</pre>';
+
+	echo '<p>High : IDR '.number_format($response['ticker']['high'], 0, ',', '.').'</p>';
+	echo '<p>Low : IDR '.number_format($response['ticker']['low'], 0, ',', '.').'</p>';
+	echo '<p>Vol BTC : '.number_format($response['ticker']['vol_btc'], 5, ',', '.').'</p>';
+	echo '<p>Vol IDR : IDR '.number_format($response['ticker']['vol_idr'], 0, ',', '.').'</p>';
+	echo '<p>Last : IDR '.number_format($response['ticker']['last'], 0, ',', '.').'</p>';
+	echo '<p>buy : IDR '.number_format($response['ticker']['buy'], 0, ',', '.').'</p>';
+	echo '<p>sell : IDR '.number_format($response['ticker']['sell'], 0, ',', '.').'</p>';
+	echo '<p>Server time : '.date('r', $response['ticker']['server_time']).'</p>';
 }
 ?>
